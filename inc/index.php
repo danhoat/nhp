47768de9495d94165a2e7b780c07755b547ba7c9
danngyw <danng@youngworld.vn>
2015-08-01 22:09:01 +0700

    update

--- inc/index.php
+++ inc/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require get_template_directory() . '/inc/comments.php';
-require get_template_directory() . '/inc/widgets.php';
 require get_template_directory() . '/inc/widgets/index.php';
 require get_template_directory() . '/inc/themes.php';
 require get_template_directory() . '/inc/customizer.php';
--- inc/widgets/index.php
+++ inc/widgets/index.php
@@ -1,5 +1,5 @@
 <?php
-
+	require get_template_directory() . '/inc/widgets/widgets.php';
 	require get_template_directory() . '/inc/widgets/category-product.php';
 	require get_template_directory() . '/inc/widgets/post-slider.php';
 ?>
